716f8bd959647abcf37774ca445ceca8f45a950a
krishnaditi <132455209+krishnaditi@users.noreply.github.com>
2025-02-19 17:12:53 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
-// import { BootstrapVueNext } from 'bootstrap-vue-next';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
+import { BootstrapVueNext } from 'bootstrap-vue-next';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 import App from './App.vue'
 import router from './router/index.js'
 
 
 const app = createApp(App)
-// app.use(BootstrapVueNext)
+app.use(BootstrapVueNext)
 app.use(router)
 app.mount('#app')
